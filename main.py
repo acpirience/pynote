c6f873c9d0e2c86206328641e73a14fbc63c70a7
Acpirience <acpirience@gmail.com>
2024-10-15 15:40:48 +0200

    datafile searching

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import pprint
+import glob
 import tomllib
 
 from loguru import logger
@@ -6,12 +6,25 @@ from loguru import logger
 
 def main() -> None:
     logger.info("Starting Pynote")
-    logger.info("Opening config.toml")
+    logger.info("Loading config")
     with open("config.toml") as fileObj:
         content = fileObj.read()
         config = tomllib.loads(content)
 
-    pprint.pprint(config)
+    logger.info("Loading datafile list")
+    datafile_list: list[str] = glob.glob(
+        f"{config["data"]["notes_dir"]}"
+        f"{config["data"]["datafile_prefix"]}"
+        f"*{config["data"]["datafile_suffix"]}"
+    )
+
+    student_list = [
+        x.removeprefix(config["data"]["notes_dir"])
+        .removeprefix(config["data"]["datafile_prefix"])
+        .removesuffix(config["data"]["datafile_suffix"])
+        for x in datafile_list
+    ]
+    logger.info(f"Student list:{student_list}")
 
 
 if __name__ == "__main__":
